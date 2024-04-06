8c034e398a95328fb8d221a7674ac16a23e9b540
eltonduarte <eltonpduarte@gmail.com>
2024-04-06 09:54:24 -0300

    Adicion template.py

--- template.py
+++ template.py
@@ -8,8 +8,8 @@ try:
     bot_web = make_browser.chrome()
     log_to_file.info('Sessão com o webdriver criada com sucesso', 'log.txt')
 
-except Exception as error:
-    log_to_file.error('Sessão com o webdriver criada com sucesso', 'log.txt')
+except Exception as error_message:
+    log_to_file.error(f'{error_message}', 'log.txt')
 
 finally:
     bot_web.close()
